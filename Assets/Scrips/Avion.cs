e08c83ae1075ef9afe7b87b1f41b9bc88426d1e7
GreckNavarro <130480169+GreckNavarro@users.noreply.github.com>
2023-05-14 22:22:39 -0500

    A4-Quaternions

--- Assets/Scrips/Avion.cs
+++ Assets/Scrips/Avion.cs
@@ -7,8 +7,17 @@ public class Avion : MonoBehaviour
 
     [SerializeField] float speed = 10.0f;
     [SerializeField] float rotationSpeed = 50.0f;
+     private Vector3 angulo;
+   private Quaternion qz = Quaternion.identity;
+   private Quaternion qx = Quaternion.identity;
+   private Quaternion qy = Quaternion.identity;
+   private Quaternion r = Quaternion.identity;
 
-   float horizontalInput;
+    private float anguloSen;
+    private float anguloCos;
+
+
+    float horizontalInput;
    float verticalInput;
 
     private void Update()
@@ -20,7 +29,33 @@ public class Avion : MonoBehaviour
     private void FixedUpdate()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        transform.Rotate(Vector3.forward * -horizontalInput * rotationSpeed * Time.deltaTime);
-        transform.Rotate(Vector3.right * verticalInput * rotationSpeed * Time.deltaTime);
+        
+        Movimiento();
+
+    }
+    public void Movimiento()
+    {
+        angulo.z += -horizontalInput * rotationSpeed * Time.deltaTime;
+       
+
+        anguloSen = Mathf.Sin(Mathf.Deg2Rad * angulo.z * 0.5f);
+        anguloCos = Mathf.Cos(Mathf.Deg2Rad * angulo.z * 0.5f);
+        qz.Set(0, 0, anguloSen, anguloCos);
+
+
+        angulo.x += -verticalInput * rotationSpeed * Time.deltaTime;
+        anguloSen = Mathf.Sin(Mathf.Deg2Rad * angulo.x * 0.5f);
+        anguloCos = Mathf.Cos(Mathf.Deg2Rad * angulo.x * 0.5f);
+        qx.Set(anguloSen, 0, 0, anguloCos);
+
+
+        angulo.y += horizontalInput * rotationSpeed * Time.deltaTime;
+        anguloSen = Mathf.Sin(Mathf.Deg2Rad * angulo.y * 0.5f);
+        anguloCos = Mathf.Cos(Mathf.Deg2Rad * angulo.y * 0.5f);
+        qy.Set(0, anguloSen, 0, anguloCos);
+
+        r = qy * qx * qz;
+
+        transform.rotation = r;
     }
 }
